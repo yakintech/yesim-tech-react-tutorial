d5e99275d03f01908371ceb1056b6b8209d92643
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-05-22 11:11:54 +0300

    delete in datagrid

--- src/muiSample/ProductsDataGrid.jsx
+++ src/muiSample/ProductsDataGrid.jsx
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { axiosInstance } from '../config/axiosInstance'
 import { DataGrid } from '@mui/x-data-grid';
-import { trTR } from '@mui/material/locale';
+import { Button, CircularProgress } from '@mui/material';
 
 
 function ProductsDataGrid() {
 
     const [products, setproducts] = useState([])
+    const [loading, setloading] = useState(true)
 
     useEffect(() => {
         loadProducts();
     }, [])
+
+
+    const deleteProduct = (id) => {
+        setloading(true)
+
+        const confirm = window.confirm("Are u sure?")
+
+        if (!confirm) {
+            setloading(false)
+            return
+        }
+        axiosInstance.delete("products/" + id)
+            .then(res => {
+                loadProducts();
+            })
+
+    }
 
 
     const columns = [
@@ -22,34 +40,53 @@ function ProductsDataGrid() {
         {
             field: "name",
             headerName: "Name",
-            width: 250,
-            renderCell: (params) => {
-                return <strong style={{color:'red'}}>{params.value.toUpperCase()}</strong>
-            }
+            width: 250
         },
         {
             field: "unitPrice",
             headerName: "Unit Price",
             width: 150
+        },
+        {
+            field: "companyName",
+            headerName: "Company Name",
+            width: 150,
+            renderCell: item => <>{item.row.supplier?.companyName}</>
+        },
+        {
+            field: "delete",
+            headerName: "Delete",
+            width: 150,
+            renderCell: item => <Button variant="contained" color="error" onClick={() => deleteProduct(item.row.id)}>Delete</Button>
         }
     ]
 
 
     const loadProducts = () => {
-        axiosInstance.get("products")
-            .then(res => {
-                setproducts(res.data)
-            })
+        setTimeout(() => {
+            axiosInstance.get("products")
+                .then(res => {
+                    setproducts(res.data)
+                    setloading(false)
+                })
+                .catch(err => {
+                    alert("Error!")
+                    setloading(false)
+                })
+        }, 2000); //loading gözükmesi için ekledim
     }
 
     return (<>
-        <div style={{ height: 600, width: '100%' }}>
-            <DataGrid
-                rows={products}// data source
-                columns={columns}
-                localeText={trTR}
-            />
-        </div>
+        {
+            loading ? <CircularProgress /> : <div style={{ height: 600, width: '100%' }}>
+                <DataGrid
+                    rows={products}// data source
+                    columns={columns}
+                    loading={loading}
+                />
+            </div>
+        }
+
 
     </>)
 }
